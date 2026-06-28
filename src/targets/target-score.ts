a1560d79f315c1d0468e473aa57953cb4646051a
denex23 <denex23@gmail.com>
2026-06-28 14:48:59 +0200

    small servers still gets to much attention

--- src/targets/target-score.ts
+++ src/targets/target-score.ts
@@ -2,7 +2,14 @@ import { ServerInfo } from "src/models/server-info"
 
 export function calculateScore(server: ServerInfo): number 
 {
-    return server.maxMoney <= 0 
-        ? 0 
-        : (server.maxMoney * server.growth * server.chance) / (server.minSecurity + 1);
+    // credits to ChatGPT which had the idea with Math.log10
+    return server.maxMoney <= 0
+        ? 0
+        : (
+            server.maxMoney 
+            * Math.log10(server.maxMoney) 
+            * Math.min(1, (server.maxMoney / 100_000_000))
+            * server.growth 
+            * server.chance
+        ) / (server.minSecurity + 1);
 }
