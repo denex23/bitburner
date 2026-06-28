b97d56a23c08cccc48061350829bc2cea8432afa
denex23 <denex23@gmail.com>
2026-06-28 14:35:07 +0200

    fix the fix ... bitburner cant start the scrpts perhaps cache :shrug:

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -29,9 +29,9 @@ export const STATE_WEIGHT: Record<TargetState, number> = {
 };
 
 export const SCRIPT_MAP: Record<WorkerAction, string> = {
-    [WorkerAction.Hack]: "src/workers/hack.js",
-    [WorkerAction.Grow]: "src/workers/grow.js",
-    [WorkerAction.Weaken]: "src/workers/weaken.js",
+    [WorkerAction.Hack]: "src/workers/hack.ts",
+    [WorkerAction.Grow]: "src/workers/grow.ts",
+    [WorkerAction.Weaken]: "src/workers/weaken.ts",
 };
 
 export const SCRIPT_RAM: Record<WorkerAction, number> = {
